a13e37280bbdee4e0d9129d55eaac531e40546ce
dalma-david-magalhaes <david.magalhaes@dalmasystems.com>
2022-12-07 00:45:57 +0000

    start app and start api

---
+++ app/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HeaderView from "./components/HeaderView.vue";
+</script>
+
+<template>
+  <header class="container mx-auto w-4/5 sticky top-10 mt-28">
+    <HeaderView />
+  </header>
+  <main>
+    <RouterView />
+  </main>
+  <footer>FOOTER</footer>
+</template>
